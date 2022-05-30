9007c57faa192d38b9203b0e8febfcf7da449cdf
Aleksandr Mihasenko <aleksandr.mihasenko@gmail.com>
2022-05-30 20:28:21 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import {useEffect, useState} from 'react';
+
 function App() {
+  const [solution, setSolution] = useState(null)
+
+  useEffect(() => {
+    fetch(' http://localhost:3001/solutions')
+      .then((response) => response.json())
+      .then((data) => {
+        const randomSolution = data[Math.floor(Math.random() * data.length)]
+
+        setSolution(randomSolution.word)
+      })
+  }, [setSolution])
+
   return (
     <div className="App">
       <h1>Wordle app</h1>
+      { solution && <div>Solution is: { solution }</div> }
     </div>
   )
 }
